b511aa75e3343b6b2172b09a8925d8c5b6f6cdbe
Adam Martin <adam.d.martin1986@gmail.com>
2021-01-19 20:15:42 -0500

    started apollo server tutorial

--- src/index.js
+++ src/index.js
@@ -1,5 +1,64 @@
-import 'dotenv/config';
+import cors from 'cors';
+import express from 'express';
+import { ApolloServer, gql } from 'apollo-server-express';
+// import 'dotenv/config';
 
-console.log ('Hello ever running Node.js project.');
+const app = express();
 
-console.log(process.env.MY_SECRET);
+app.use(cors());
+
+const schema = gql`
+    type Query {
+        users: [User!]
+        me:User
+        user(id: ID!): User
+    }
+
+    type User {
+        id: ID!
+        username: String!
+        }`;
+
+let users = {
+    1: {
+        id: '1',
+        username: 'Adam Martin'
+    },
+    2: {
+        id: '2',
+        username: 'Jonathan Barclay',
+    }
+}
+const me = users[1];
+
+const resolvers = {
+    Query: {
+        users: () => {
+            return Object.values(users);
+        },
+        user: (parent, { id }) => {
+            return users[id];
+        },
+        me: () => {
+            return me;
+        },
+    },
+}
+
+const server = new ApolloServer({
+    typeDefs: schema,
+    resolvers,
+});
+
+
+
+server.applyMiddleware({ app, path: '/graphql' });
+
+app.listen({ port: 8000 }, () => {
+    console.log('Apollo Server on http://localhost:8000/graphql');
+})
+
+
+// console.log ('Hello ever running Node.js project.');
+
+// console.log(process.env.MY_SECRET);
